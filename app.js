6b0fb914b865a71ec1b625bf05648675fc8e5f0e
Tyler Norman <ty@Tys-MacBook-Air-2.local>
2026-03-29 21:48:26 -0400

    Finished

--- app.js
+++ app.js
@@ -1,38 +1,24 @@
-const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
 
-const app = express();
-app.use(cors());
-app.use(express.json());
+//testdb is name of database, it will automatically make it
+mongoose
+  .connect("mongodb+srv://tylernormdb:backendyay@backend.bmj4mvh.mongodb.net/?appName=Backend")
+  .then(() => console.log("Connected to mongodb..."))
+  .catch((err) => console.error("could not connect ot mongodb...", err));
 
-// Connect to MongoDB Atlas
-mongoose.connect("mongodb+srv://tylernormdb:backendyay@backend.bmj4mvh.mongodb.net/?appName=Backend")
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
-
-// Schema
-const ItemSchema = new mongoose.Schema({
-  name: String
+const schema = new mongoose.Schema({
+  name: String,
 });
 
-const Item = mongoose.model("Item", ItemSchema);
+async function createMessage() {
+  const result = await message.save();
+  console.log(result);
+}
 
-// Routes
-app.get("/", (req, res) => {
-  res.send("API is running");
+//this creates a Message class in our app
+const Message = mongoose.model("Message", schema);
+const message = new Message({
+  name: "Hello World",
 });
 
-app.get("/items", async (req, res) => {
-  const items = await Item.find();
-  res.json(items);
-});
-
-app.post("/items", async (req, res) => {
-  const newItem = new Item(req.body);
-  await newItem.save();
-  res.json(newItem);
-});
-
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("Server running"));
+createMessage();
