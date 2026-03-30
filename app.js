4791ee7dfcb4ea5b6c94a388fb14d64b155b05a3
Tyler Norman <ty@Tys-MacBook-Air-2.local>
2026-03-29 21:33:35 -0400

    Backend DB

--- app.js
+++ app.js
@@ -1,9 +1,38 @@
-var http = require("http");
-const port = process.env.PORT || 3001;
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
-http
-  .createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.end("Hello World!");
-  })
-  .listen(port);
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB Atlas
+mongoose.connect("mongodb+srv://tylernormdb:backendyay@backend.bmj4mvh.mongodb.net/?appName=Backend")
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+// Schema
+const ItemSchema = new mongoose.Schema({
+  name: String
+});
+
+const Item = mongoose.model("Item", ItemSchema);
+
+// Routes
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.get("/items", async (req, res) => {
+  const items = await Item.find();
+  res.json(items);
+});
+
+app.post("/items", async (req, res) => {
+  const newItem = new Item(req.body);
+  await newItem.save();
+  res.json(newItem);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running"));
